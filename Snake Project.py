efdfd5274b8d21bbf1dfdd6361905fa1610541a7
caesarsalad64 <ccuevas@hawk.iit.edu>
2017-10-12 12:03:48 -0500

    Add files via upload

--- Snake Project.py
+++ Snake Project.py
@@ -1,4 +1,4 @@
-from sense_hat import SenseHat
+from sense_hat import SenseHat # it is sense_hat when using pi
 from time import sleep
 from random import randint
 
@@ -7,7 +7,7 @@ sense = SenseHat()
 # Variables ---------------------------
 slug = [[2,4],[3,4],[4,4]]
 color = (124, 193, 78)
-direction = "Right"
+direction = "right"
 blank = (0,0,0)
 
 # Functions ---------------------------
@@ -22,12 +22,30 @@ def move():
   next = list(last)     # Create a copy of the last item
 
   # Find the next pixel in the direction the slug is currently moving
+
   if direction == "right":
-      # Move along the column
-      if last[0] + 1 == 8:
-          next[0] = 0
-      else:
-          next[0] = last[0] + 1
+        if last[0] + 1 == 8:
+            next[0] = 0
+        else:
+            next[0] = last[0] + 1
+
+  elif direction == "left":
+        if last[0] - 1 == -1:
+            next[0] = 7
+        else:
+            next[0] = last[0] - 1
+
+  elif direction == "down":
+        if last[1] + 1 == 8:
+            next[1] = 0
+        else:
+            next[1] = last[1] + 1
+
+  elif direction == "up":
+        if last[1] - 1 == -1:
+            next[1] = 7
+        else:
+            next[1] = last[1] - 1
 
   # Add this pixel at the end of the slug list
   slug.append(next)
@@ -41,25 +59,15 @@ def move():
   # Remove the first pixel from the list
   slug.remove(first)
 
-def wrap(pix):
-    # Wrap x coordinate
-    if pix[0] > 7:
-        pix[0] = 0
-    if pix[0] < 0:
-        pix[0] = 7
-    # Wrap y coordinate
-    if pix[1] < 0:
-        pix[1] = 7
-    if pix[1] > 7:
-        pix[1] = 0
+def joystick_moved(event):
+    global direction
+    direction = event.direction
 
-    return pix
 
 # Main program ------------------------
 sense.clear()
 draw_slug()
+sense.stick.direction_any = joystick_moved
 while True:
   move()
   sleep(0.5)
-wrap()
-

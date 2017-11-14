a375ca8fb05e25fd67b4177015ff8911179a73dc
Kcho12 <32373546+Kcho12@users.noreply.github.com>
2017-11-14 12:55:05 -0500

    New improved "movement"
    Code will no longer move the array, but will instead play an animation using different arrays to simulate movement.

--- CS-100 Final project Movement.py
+++ CS-100 Final project Movement.py
@@ -2,41 +2,32 @@
 ## Main pitch measurement is imported from https://github.com/TeCoEd/Egg-Drop by dan_aldred
 
 from sense_hat import SenseHat
+from random import randint
 
 sense = SenseHat()
 sense.clear()
 import time
 
 game_over = False
-basket_x = 7
 
-'''main pitch measurement'''
-def basket_move(pitch, basket_x):
-    sense.set_pixel(basket_x, 7, [0, 0, 0])
-    new_x = basket_x
-    if 1 < pitch < 179 and basket_x != 0:
-        new_x -= 1
-    elif 359 > pitch > 179 and basket_x != 7:
-        new_x += 1
-    return new_x,
-
-'''Main game setup'''
-def main():
-
-    basket_x = 3
-       
-    while game_over == False:
-        pitch = sense.get_orientation()['pitch'] ##obtains gyroscope readings
-        basket_x, = basket_move(pitch, basket_x) ##moves the Sprite
-        
-        '''Set Basket Positon'''
-        sense.set_pixel(basket_x, 7, [139, 69, 19]) ## (x,y,color)
-        time.sleep(0.2)
-         
-   
-
-
-        
-main()
-time.sleep(1)
-sense.clear()
+# Functions ------------------------------------------------
+'''main move function to play the animation'''
+def move_place():
+      if pitch == 0:
+        sense.set_pixels(tom_1)
+      if 359 > pitch > 179:
+          sense.set_pixels(tom_1)
+          time.sleep(0.2)
+          sense.set_pixels(tom_2)
+          time.sleep(0.2)
+      elif 1 < pitch < 179:
+          sense.set_pixels(tom_3)
+          time.sleep(0.2)
+          sense.set_pixels(tom_4)          
+          time.sleep(0.2)
+          
+## Main ----------------------------------------------------------
+while game_over == False:
+  pitch = sense.get_orientation()['pitch'] ##Obtains the gyroscope readings
+  move_place()
+  time.sleep(1)

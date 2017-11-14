4daf4e5ebf0d601d0b564240f30dc716832ec4d0
caesarsalad64 <ccuevas@hawk.iit.edu>
2017-11-14 12:31:44 -0600

    Update Tomagotchi.py

--- Tomagotchi.py
+++ Tomagotchi.py
@@ -11,17 +11,95 @@ y = (253,184,19) # yellow
 b = (0,191,255) # blue
 color_two = (255,0,0) # idk this color
 l = (0,0,0) # blank
+variable = True
 
 # Functions ---------------------------
-tom_pixels = [
-    l, l, l, l, l, l, l, l,
-    l, l, l, l, l, br, l, l,
+tom_pixels_right_1 = [
+    y, y, l, l, l, l, l, l,
+    y, y, l, l, l, br, l, l,
     l, l, l, l, l, br, br, l,
     l, l, br, br, br, br, l, l,
     l, br, br, br, br, br, l, l,
     l, l, br, l, l, br, l, l,
     l, l, br, l, l, br, l, l,
-    l, l, l, l, l, l, l, l,
+    g, g, g, g, g, g, g, g,
+    ]
+
+tom_pixels_right_2 = [
+    y, y, l, l, l, l, l, l,
+    y, y, l, l, l, l, br, l,
+    l, l, l, l, l, l, br, br,
+    l, l, l, br, br, br, br, l,
+    l, l, br, br, br, br, br, l,
+    l, l, l, br, l, l, br, l,
+    l, l, l, br, l, l, br, l,
+    g, g, g, g, g, g, g, g,
+    ]
+
+tom_pixels_right_3 = [
+    y, y, l, l, l, l, l, l,
+    y, y, l, l, l, l, l, br,
+    br, l, l, l, l, l, l, br,
+    l, l, l, l, br, br, br, br,
+    l, l, l, br, br, br, br, br,
+    l, l, l, l, br, l, l, br,
+    l, l, l, l, br, l, l, br,
+    g, g, g, g, g, g, g, g,
+    ]
+
+tom_pixels_right_4 = [
+    y, y, l, l, l, l, l, l,
+    br, y, l, l, l, l, l, l,
+    br, br, l, l, l, l, l, l,
+    br, l, l, l, l, br, br, br,
+    br, l, l, l, br, br, br, br,
+    br, l, l, l, l, br, l, l,
+    br, l, l, l, l, br, l, l,
+    g, g, g, g, g, g, g, g,
+    ]
+
+tom_pixels_right_5 = [
+    y, y, l, l, l, l, l, l,
+    y, br, l, l, l, l, l, l,
+    l, br, br, l, l, l, l, l,
+    br, br, l, l, l, l, br, br,
+    br, br, l, l, l, br, br, br,
+    l, br, l, l, l, l, br, l,
+    l, br, l, l, l, l, br, l,
+    g, g, g, g, g, g, g, g,
+    ]
+
+tom_pixels_right_6 = [
+    y, y, l, l, l, l, l, l,
+    y, y, br, l, l, l, l, l,
+    l, l, br, br, l, l, l, l,
+    br, br, br, l, l, l, l, br,
+    br, br, br, l, l, l, br, br,
+    l, l, br, l, l, l, l, br,
+    l, l, br, l, l, l, l, br,
+    g, g, g, g, g, g, g, g,
+    ]
+
+tom_pixels_right_7 = [
+    y, y, l, l, l, l, l, l,
+    y, y, l, br, l, l, l, l,
+    l, l, l, br, br, l, l, l,
+    br, br, br, br, l, l, l, l,
+    br, br, br, br, l, l, l, br,
+    br, l, l, br, l, l, l, l,
+    br, l, l, br, l, l, l, l,
+    g, g, g, g, g, g, g, g,
+    ]
+
+tom_pixels_right_8 = [
+    y, y, l, l, l, l, l, l,
+    y, y, l, l, br, l, l, l,
+    l, l, l, l, br, br, l, l,
+    l, br, br, br, br, l, l, l,
+    br, br, br, br, br, l, l, l,
+    l, br, l, l, br, l, l, l,
+    l, br, l, l,br, l, l, l,
+    g, g, g, g, g, g, g, g,
     ]
 
 tom_pixels_with_setting = [
@@ -35,6 +113,132 @@ tom_pixels_with_setting = [
     g, g, g, g, g, g, g, g,
     ]
 
+tom_pixels_left_1 = [
+    y, y, l, l, l, l, l, l,
+    y, y, br, l, l, l, l, l,
+    l, br, br, l, l, l, l, l,
+    l, l, br, br, br, br, l, l,
+    l, l, br, br, br, br, br, l,
+    l, l, br, l, l, br, l, l,
+    l, l, br, l, l, br, l, l,
+    g, g, g, g, g, g, g, g,
+    ]
+    
+tom_pixels_left_2 = [
+    y, y, l, l, l, l, l, l,
+    y, br, l, l, l, l, l, l,
+    br, br, l, l, l, l, l, l,
+    l, br, br, br, br, l, l, l,
+    l, br, br, br, br, br, l, l,
+    l, br, l, l, br, l, l, l,
+    l, br, l, l, br, l, l, l,
+    g, g, g, g, g, g, g, g,
+    ]
+
+tom_pixels_left_3 = [
+    y, y, l, l, l, l, l, l,
+    br, y, l, l, l, l, l, l,
+    br, l, l, l, l, l, l, br,
+    br, br, br, br, l, l, l, l,
+    br, br, br, br, br, l, l, l,
+    br, l, l, br, l, l, l, l,
+    br, l, l, br, l, l, l, l,
+    g, g, g, g, g, g, g, g,
+    ]
+    
+tom_pixels_left_4 = [
+    y, y, l, l, l, l, l, l,
+    y, y, l, l, l, l, l, br,
+    l, l, l, l, l, l, br, br,
+    br, br, br, l, l, l, l, br,
+    br, br, br, br, l, l, l, br,
+    l, l, br, l, l, l, l, br,
+    l, l, br, l, l, l, l, br,
+    g, g, g, g, g, g, g, g,
+    ]
+    
+tom_pixels_left_5 = [
+    y, y, l, l, l, l, l, l,
+    y, y, l, l, l, l, br, l,
+    l, l, l, l, l, br, br, l,
+    br, br, l, l, l, l, br, br,
+    br, br, br, l, l, l, br, br,
+    l, br, l, l, l, l, br, l,
+    l, br, l, l, l, l, br, l,
+    g, g, g, g, g, g, g, g,
+    ]
+    
+tom_pixels_left_6 = [
+    y, y, l, l, l, l, l, l,
+    y, y, l, l, l, br, l, l,
+    l, l, l, l, br, br, l, l,
+    br, l, l, l, l, br, br, br,
+    br, br, l, l, l, br, br, br,
+    br, l, l, l, l, br, l, l,
+    br, l, l, l, l, br, l, l,
+    g, g, g, g, g, g, g, g,
+    ]
+    
+tom_pixels_left_7 = [
+    y, y, l, l, l, l, l, l,
+    y, y, l, l, br, l, l, l,
+    l, l, l, br, br, l, l, l,
+    l, l, l, l, br, br, br, br,
+    br, l, l, l, br, br, br, br,
+    l, l, l, l, br, l, l, br,
+    l, l, l, l, br, l, l, br,
+    g, g, g, g, g, g, g, g,
+    ]
+    
+tom_pixels_left_8 = [
+    y, y, l, l, l, l, l, l,
+    y, y, l, br, l, l, l, l,
+    l, l, br, br, l, l, l, l,
+    l, l, l, br, br, br, br, l,
+    l, l, l, br, br, br, br, br,
+    l, l, l, br, l, l, br, l,
+    l, l, l, br, l, l, br, l,
+    g, g, g, g, g, g, g, g,
+    ]
+
+def draw_tom_right():
+  sense.set_pixels(tom_pixels_right_1)
+  sleep(.15)
+  sense.set_pixels(tom_pixels_right_2)
+  sleep(.15)
+  sense.set_pixels(tom_pixels_right_3)
+  sleep(.15)
+  sense.set_pixels(tom_pixels_right_4)
+  sleep(.15)
+  sense.set_pixels(tom_pixels_right_5)
+  sleep(.15)
+  sense.set_pixels(tom_pixels_right_6)
+  sleep(.15)
+  sense.set_pixels(tom_pixels_right_7)
+  sleep(.15)
+  sense.set_pixels(tom_pixels_right_8)
+  sleep(.15)
+  
+def draw_tom_left():
+  sense.set_pixels(tom_pixels_left_1)
+  sleep(.15)
+  sense.set_pixels(tom_pixels_left_2)
+  sleep(.15)
+  sense.set_pixels(tom_pixels_left_3)
+  sleep(.15)
+  sense.set_pixels(tom_pixels_left_4)
+  sleep(.15)
+  sense.set_pixels(tom_pixels_left_5)
+  sleep(.15)
+  sense.set_pixels(tom_pixels_left_6)
+  sleep(.15)
+  sense.set_pixels(tom_pixels_left_7)
+  sleep(.15)
+  sense.set_pixels(tom_pixels_left_8)
+  sleep(.15)
+  
+
 # Main program ------------------------
 sense.clear()
-sense.set_pixels(tom_pixels_with_setting)
+while(variable):
+  draw_tom_left()

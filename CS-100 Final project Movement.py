d18c38f692a1373116723bfc71ab754ff2b75021
Kcho12 <32373546+Kcho12@users.noreply.github.com>
2017-11-16 13:19:34 -0500

    It works!
    Contains all the sprites for left and right movement.

--- CS-100 Final project Movement.py
+++ CS-100 Final project Movement.py
@@ -1,5 +1,4 @@
-## This code is reponsible for the movement of the sprite utilizing the gyroscope.
-## Main pitch measurement is imported from https://github.com/TeCoEd/Egg-Drop by dan_aldred
+# This code is reponsible for the animation of the sprite utilizing the gyroscope.
 
 from sense_hat import SenseHat
 from random import randint
@@ -8,26 +7,248 @@ sense = SenseHat()
 sense.clear()
 import time
 
-game_over = False
+# Variables ---------------------------
+br = (244,164,96) # Brown
+g = (0,104,10) # green
+y = (253,184,19) # yellow
+b = (0,191,255) # blue
+color_two = (255,0,0) # idk this color
+l = (0,0,0) # blank
+temp = sense.get_temperature()
 
+tom_pixels_right_1 = [
+    y, y, l, l, l, l, l, l,
+    y, y, l, l, l, br, l, l,
+    l, l, l, l, l, br, br, l,
+    l, l, br, br, br, br, l, l,
+    l, br, br, br, br, br, l, l,
+    l, l, br, l, l, br, l, l,
+    l, l, br, l, l, br, l, l,
+    g, g, g, g, g, g, g, g,
+    ]
+
+tom_pixels_right_2 = [
+    y, y, l, l, l, l, l, l,
+    y, y, l, l, l, l, br, l,
+    l, l, l, l, l, l, br, br,
+    l, l, l, br, br, br, br, l,
+    l, l, br, br, br, br, br, l,
+    l, l, l, br, l, l, br, l,
+    l, l, l, br, l, l, br, l,
+    g, g, g, g, g, g, g, g,
+    ]
+
+tom_pixels_right_3 = [
+    y, y, l, l, l, l, l, l,
+    y, y, l, l, l, l, l, br,
+    br, l, l, l, l, l, l, br,
+    l, l, l, l, br, br, br, br,
+    l, l, l, br, br, br, br, br,
+    l, l, l, l, br, l, l, br,
+    l, l, l, l, br, l, l, br,
+    g, g, g, g, g, g, g, g,
+    ]
+
+tom_pixels_right_4 = [
+    y, y, l, l, l, l, l, l,
+    br, y, l, l, l, l, l, l,
+    br, br, l, l, l, l, l, l,
+    br, l, l, l, l, br, br, br,
+    br, l, l, l, br, br, br, br,
+    br, l, l, l, l, br, l, l,
+    br, l, l, l, l, br, l, l,
+    g, g, g, g, g, g, g, g,
+    ]
+
+tom_pixels_right_5 = [
+    y, y, l, l, l, l, l, l,
+    y, br, l, l, l, l, l, l,
+    l, br, br, l, l, l, l, l,
+    br, br, l, l, l, l, br, br,
+    br, br, l, l, l, br, br, br,
+    l, br, l, l, l, l, br, l,
+    l, br, l, l, l, l, br, l,
+    g, g, g, g, g, g, g, g,
+    ]
+
+tom_pixels_right_6 = [
+    y, y, l, l, l, l, l, l,
+    y, y, br, l, l, l, l, l,
+    l, l, br, br, l, l, l, l,
+    br, br, br, l, l, l, l, br,
+    br, br, br, l, l, l, br, br,
+    l, l, br, l, l, l, l, br,
+    l, l, br, l, l, l, l, br,
+    g, g, g, g, g, g, g, g,
+    ]
+
+tom_pixels_right_7 = [
+    y, y, l, l, l, l, l, l,
+    y, y, l, br, l, l, l, l,
+    l, l, l, br, br, l, l, l,
+    br, br, br, br, l, l, l, l,
+    br, br, br, br, l, l, l, br,
+    br, l, l, br, l, l, l, l,
+    br, l, l, br, l, l, l, l,
+    g, g, g, g, g, g, g, g,
+    ]
+
+tom_pixels_right_8 = [
+    y, y, l, l, l, l, l, l,
+    y, y, l, l, br, l, l, l,
+    l, l, l, l, br, br, l, l,
+    l, br, br, br, br, l, l, l,
+    br, br, br, br, br, l, l, l,
+    l, br, l, l, br, l, l, l,
+    l, br, l, l,br, l, l, l,
+    g, g, g, g, g, g, g, g,
+    ]
+
+tom_pixels_with_setting = [
+    y, y, b, b, b, b, b, b,
+    y, y, b, b, b, br, b, b,
+    b, b, b, b, b, br, br, b,
+    b, b, br, br, br, br, b, b,
+    b, br, br, br, br, br, b, b,
+    b, b, br, b, b, br, b, b,
+    b, b, br, b, b, br, b, b,
+    g, g, g, g, g, g, g, g,
+    ]
+
+tom_pixels_left_1 = [
+    y, y, l, l, l, l, l, l,
+    y, y, br, l, l, l, l, l,
+    l, br, br, l, l, l, l, l,
+    l, l, br, br, br, br, l, l,
+    l, l, br, br, br, br, br, l,
+    l, l, br, l, l, br, l, l,
+    l, l, br, l, l, br, l, l,
+    g, g, g, g, g, g, g, g,
+    ]
+    
+tom_pixels_left_2 = [
+    y, y, l, l, l, l, l, l,
+    y, br, l, l, l, l, l, l,
+    br, br, l, l, l, l, l, l,
+    l, br, br, br, br, l, l, l,
+    l, br, br, br, br, br, l, l,
+    l, br, l, l, br, l, l, l,
+    l, br, l, l, br, l, l, l,
+    g, g, g, g, g, g, g, g,
+    ]
+
+tom_pixels_left_3 = [
+    y, y, l, l, l, l, l, l,
+    br, y, l, l, l, l, l, l,
+    br, l, l, l, l, l, l, br,
+    br, br, br, br, l, l, l, l,
+    br, br, br, br, br, l, l, l,
+    br, l, l, br, l, l, l, l,
+    br, l, l, br, l, l, l, l,
+    g, g, g, g, g, g, g, g,
+    ]
+    
+tom_pixels_left_4 = [
+    y, y, l, l, l, l, l, l,
+    y, y, l, l, l, l, l, br,
+    l, l, l, l, l, l, br, br,
+    br, br, br, l, l, l, l, br,
+    br, br, br, br, l, l, l, br,
+    l, l, br, l, l, l, l, br,
+    l, l, br, l, l, l, l, br,
+    g, g, g, g, g, g, g, g,
+    ]
+    
+tom_pixels_left_5 = [
+    y, y, l, l, l, l, l, l,
+    y, y, l, l, l, l, br, l,
+    l, l, l, l, l, br, br, l,
+    br, br, l, l, l, l, br, br,
+    br, br, br, l, l, l, br, br,
+    l, br, l, l, l, l, br, l,
+    l, br, l, l, l, l, br, l,
+    g, g, g, g, g, g, g, g,
+    ]
+    
+tom_pixels_left_6 = [
+    y, y, l, l, l, l, l, l,
+    y, y, l, l, l, br, l, l,
+    l, l, l, l, br, br, l, l,
+    br, l, l, l, l, br, br, br,
+    br, br, l, l, l, br, br, br,
+    br, l, l, l, l, br, l, l,
+    br, l, l, l, l, br, l, l,
+    g, g, g, g, g, g, g, g,
+    ]
+    
+tom_pixels_left_7 = [
+    y, y, l, l, l, l, l, l,
+    y, y, l, l, br, l, l, l,
+    l, l, l, br, br, l, l, l,
+    l, l, l, l, br, br, br, br,
+    br, l, l, l, br, br, br, br,
+    l, l, l, l, br, l, l, br,
+    l, l, l, l, br, l, l, br,
+    g, g, g, g, g, g, g, g,
+    ]
+    
+tom_pixels_left_8 = [
+    y, y, l, l, l, l, l, l,
+    y, y, l, br, l, l, l, l,
+    l, l, br, br, l, l, l, l,
+    l, l, l, br, br, br, br, l,
+    l, l, l, br, br, br, br, br,
+    l, l, l, br, l, l, br, l,
+    l, l, l, br, l, l, br, l,
+    g, g, g, g, g, g, g, g,
+    ]
+
+game_over = False
+   
 # Functions ------------------------------------------------
 '''main move function to play the animation'''
 def move_place():
       if pitch == 0:
-        sense.set_pixels(tom_1)
+        sense.set_pixels(tom_pixels_with_setting)
       if 359 > pitch > 179:
-          sense.set_pixels(tom_1)
+          sense.set_pixels(tom_pixels_right_1)
           time.sleep(0.2)
-          sense.set_pixels(tom_2)
+          sense.set_pixels(tom_pixels_right_2)
           time.sleep(0.2)
+          sense.set_pixels(tom_pixels_right_3)
+          time.sleep(0.2)
+          sense.set_pixels(tom_pixels_right_4)
+          time.sleep(0.2)
+          sense.set_pixels(tom_pixels_right_5)
+          time.sleep(0.2)
+          sense.set_pixels(tom_pixels_right_6)
+          time.sleep(0.2)
+          sense.set_pixels(tom_pixels_right_7)
+          time.sleep(0.2)
+          sense.set_pixels(tom_pixels_right_8)
+          time.sleep(0.2)
+          
       elif 1 < pitch < 179:
-          sense.set_pixels(tom_3)
+          sense.set_pixels(tom_pixels_left_1)
           time.sleep(0.2)
-          sense.set_pixels(tom_4)          
+          sense.set_pixels(tom_pixels_left_2)          
+          time.sleep(0.2)
+          sense.set_pixels(tom_pixels_left_3)          
+          time.sleep(0.2)
+          sense.set_pixels(tom_pixels_left_4)          
+          time.sleep(0.2)
+          sense.set_pixels(tom_pixels_left_5)          
+          time.sleep(0.2)
+          sense.set_pixels(tom_pixels_left_6)          
+          time.sleep(0.2)
+          sense.set_pixels(tom_pixels_left_7)          
+          time.sleep(0.2)
+          sense.set_pixels(tom_pixels_left_8)          
           time.sleep(0.2)
           
 ## Main ----------------------------------------------------------
 while game_over == False:
   pitch = sense.get_orientation()['pitch'] ##Obtains the gyroscope readings
   move_place()
-  time.sleep(1)
+  time.sleep(0.1)
+  
